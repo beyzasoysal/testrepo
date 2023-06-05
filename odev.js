91332853be15c00a8fc16e5f5d75324691b63c0c
beyzasoysal <beyzademirci_@hotmail.com>
2023-06-05 15:40:31 +0300

    initiliazed

---
+++ odev.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("addButton");
+console.log(button);
+
+const newButton = document.querySelector("#addButton");
+newButton.addEventListener("click",function(){
+    prompt("Beyza");
+});
+
+
+const carlist = document.getElementById("list");
+console.log(carlist);
